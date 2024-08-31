e055b62324c97377ea18939c6fe042737eb6798f
Fernando Suares <fernandoaqs@outlook.com.br>
2024-08-31 19:22:25 -0300

    add: run script

---
+++ start.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+RED='\e[1;91m'
+GREEN='\e[1;92m'
+BLUE='\e[1;94m'
+ORANGE='\e[1;93m'
+NO_COLOR='\e[0m'
+
+source ./scripts/aptitude.sh
+source ./scripts/flatpak.sh
+source ./scripts/gnomeExtensions.sh
+source ./scripts/shell.sh
+source ./scripts/asdf.sh
+source ./customization/run.sh
+
+echo -e "${ORANGE}This step will update system and clean caches${NO_COLOR}"
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+upgrade_cleanup
+
+echo -e "${ORANGE}Next step will remove unsude apps${NO_COLOR}"
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+remove_unused_apps
+
+echo -e "${ORANGE}Next step will install apt packages${NO_COLOR}"
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+install_deb_apps
+
+echo -e "${ORANGE}Next step will install flatpak and apps${NO_COLOR}"
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+install_flatpak_apps
+
+echo -e "${ORANGE}Next step will install gnome extensions${NO_COLOR}"
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+install_gnome_extensions
+
+echo -e "${ORANGE}Next step will config is zsh${NO_COLOR}"
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+configure_shell
+
+# echo -e "${ORANGE}Next step will config is asdf vm${NO_COLOR}"
+# read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+# configure_asdf
