6b0eb700ca1842d14f99ccb9610b823e669aad2d
Fernando Suares <fernandoaqs@outlook.com.br>
2025-01-15 22:29:13 -0300

    change gtk theme

--- customization/scripts/theme.sh
+++ customization/scripts/theme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-install_gtk_theme(){
+install_gtk_theme_tokyo_night(){
 	git clone https://github.com/Fausto-Korpsvart/Tokyonight-GTK-Theme.git
 
 	./Tokyonight-GTK-Theme/themes/install.sh --libadwaita -t purple -c dark -s compact --tweaks black
@@ -18,10 +18,29 @@ install_gtk_theme(){
 	sudo flatpak override --filesystem=xdg-config/gtk-4.0
 }
 
+
+install_gtk_theme(){
+	git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
+
+	./Colloid-gtk-theme/install.sh --tweaks float --libadwaita -s compact
+	sudo rm -rf Colloid-gtk-theme
+
+	sudo cp -r $HOME/.themes/* /usr/share/themes
+
+	gsettings set org.gnome.desktop.interface gtk-theme "Colloid-Dark-Compact"
+	gsettings set org.gnome.desktop.wm.preferences theme "Colloid-Dark-Compact"
+
+	# Setting theme icons to flatpak apps
+	sudo flatpak override --filesystem=$HOME/.themes
+	sudo flatpak override --filesystem=$HOME/.icons
+	flatpak override --user --filesystem=xdg-config/gtk-4.0
+	sudo flatpak override --filesystem=xdg-config/gtk-4.0
+}
+
 change_wallpaper(){
 	sudo cp -r ./.dotfiles/wallpapers /usr/share/backgrounds/
-	gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/wallpapers/11.jpg
-	gsettings set org.gnome.desktop.background picture-uri-dark file:///usr/share/backgrounds/wallpapers/11.jpg
+	gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/wallpapers/58.jpg
+	gsettings set org.gnome.desktop.background picture-uri-dark file:///usr/share/backgrounds/wallpapers/58.jpg
 }
 
 export change_wallpaper
