9cde0ceb9ebbb42c41a7e12fab277c47981ec191
Fernando Suares <fernandoaqs@outlook.com.br>
2025-01-15 23:03:16 -0300

    fix wezterm install

--- scripts/aptitude.sh
+++ scripts/aptitude.sh
@@ -100,9 +100,9 @@ install_deb_apps(){
 
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	wget https://github.com/wez/wezterm/releases/latest/download/WezTerm-debian.ubuntu-latest.deb
-	sudo apt install ./WezTerm-debian.ubuntu-latest.deb
-	sudo rm -rf WezTerm-debian.ubuntu-latest.deb
+	curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
+	echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+
 
 	for PKG in ${DEB_PKGS[@]}; do
 		if ! dpkg -l | grep -q $PKG; then
